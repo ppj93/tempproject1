c69f15a0ecf91237d8c27f806679dfb0f8523aec
Pravin <pravinjoshi95@gmail.com>
2017-04-28 00:03:02 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// Code goes here
+
+angular.module('myApp', ['ngMaterial', 'jkAngularCarousel'])
+
+.controller('MyCtrl', function($scope) {
+    $scope.dataArray = [
+      {
+        src: 'images/1.jpg'
+      },
+      {
+        src: 'images/2.jpg'
+      },
+      {
+        src: 'images/3.jpg'
+      },
+      {
+        src: 'images/4.jpg'
+      },
+      {
+        src: 'images/5.jpg'
+      }
+    ];
+});
