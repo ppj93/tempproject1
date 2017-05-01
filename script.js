198fd684cd8bccc4c40944e2fc7b598641856872
Pravin <pravinjoshi95@gmail.com>
2017-05-01 13:19:27 +0530

    looks like all features working

--- script.js
+++ script.js
@@ -19,11 +19,20 @@ angular.module('myApp', ['ngMaterial', 'jkAngularCarousel'])
       {
         src: 'images/5.jpg'
       },
-	  {
-        src: 'images/1.jpg'
+      {
+        src: 'images/7.png'
       },
       {
-        src: 'images/2.jpg'
+        src: 'images/3.jpg'
+      },
+      {
+        src: 'images/4.jpg'
+      },
+      {
+        src: 'images/7.png'
+      },
+	  {
+        src: 'images/1.jpg'
       },
       {
         src: 'images/3.jpg'
@@ -34,20 +43,9 @@ angular.module('myApp', ['ngMaterial', 'jkAngularCarousel'])
       {
         src: 'images/5.jpg'
       },
-	  {
-        src: 'images/1.jpg'
-      },
+      ,
       {
-        src: 'images/2.jpg'
-      },
-      {
-        src: 'images/3.jpg'
-      },
-      {
-        src: 'images/4.jpg'
-      },
-      {
-        src: 'images/5.jpg'
+        src: 'images/7.png'
       }
     ];
 });
